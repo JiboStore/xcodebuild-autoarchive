cc44f5eb1357a8a8297e9438a42d0c693f31d3f7
bzbee-gitlab <bzbee-gitlab@igg.com>
2016-12-14 12:12:48 +0800

    [xcodebuild] export archive [wip]
    Ref: https://stoneofarc.wordpress.com/2012/12/11/automatically-build-archive-and-distribute-to-testflight/

--- autobuild.sh
+++ autobuild.sh
@@ -21,20 +21,35 @@ PROVISION_PROFILE="./provisioning/CD_Dev.mobileprovision"
 ARCH_XCODE_FOLDER=~/Library/Developer/Xcode/Archives
 ARCH_DATE_FOLDER=`date '+%Y-%m-%d'`
 
+EXPORT_PLIST=appstore.plist
+SIGNED_OUT_DIR="${IPA_LOC}/signed"
+
 # start
 rm -rf "${IPA_LOC}"
+
+BUILD_SCRIPT_DIR=`pwd`
+
+echo "${BUILD_SCRIPT_DIR}"
 
 cd ${PROJ_DIR}
 xcodebuild -target ${PROJ_NAME} -sdk iphoneos -configuration ${CONFIGURATION} -xcconfig build-dir.xcconfig
 xcodebuild archive -project "${PROJ_NAME}.xcodeproj" -scheme ${SCHEME_NAME} -xcconfig build-dir.xcconfig
 
-CURRENT_DIR=`pwd`
+cp "${BUILD_SCRIPT_DIR}/appstore.plist" ${IPA_LOC}
 
 cd ${ARCH_XCODE_FOLDER}
 cd ${ARCH_DATE_FOLDER}
 
 LATEST_FILE=`ls -rt | tail -1`
 cp -R "${LATEST_FILE}" "${IPA_LOC}"
+
+cd "${IPA_LOC}"
+
+echo "CURRENT DIR = `pwd`"
+echo "LATEST_FILE = ${LATEST_FILE}"
+#cd "${LATEST_FILE}/Products/Applications"
+#xcrun -sdk iphoneos PackageApplication -v ${APP_NAME} -o ${IPA_LOC}/${IPA_NAME} --sign ${DEVELOPER_NAME} --embed ${PROVISION_PROFILE}
+xcodebuild -exportArchive -archivePath "${LATEST_FILE}" -exportOptionsPlist ${EXPORT_PLIST} -exportPath "${SIGNED_OUT_DIR}"
 
 # ok - ori
 #xcodebuild -target ${PROJ_NAME} -sdk iphoneos -configuration ${CONFIGURATION}
