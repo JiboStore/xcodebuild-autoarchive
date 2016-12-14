55f3f2c0c08bd556f359547a8b24b8d4f0866b91
bzbee-gitlab <bzbee-gitlab@igg.com>
2016-12-14 11:37:52 +0800

    [xcodebuild] grab the xcodearchive from archive directory

--- autobuild.sh
+++ autobuild.sh
@@ -17,9 +17,24 @@ IPA_NAME=casinodeluxebyigg.ipa
 DEVELOPER_NAME="casino bzbee igg (QSKC8ABWRD)"
 PROVISION_PROFILE="./provisioning/CD_Dev.mobileprovision"
 
+# archive folder
+ARCH_XCODE_FOLDER=~/Library/Developer/Xcode/Archives
+ARCH_DATE_FOLDER=`date '+%Y-%m-%d'`
+
+# start
+rm -rf "${IPA_LOC}"
+
 cd ${PROJ_DIR}
 xcodebuild -target ${PROJ_NAME} -sdk iphoneos -configuration ${CONFIGURATION} -xcconfig build-dir.xcconfig
 xcodebuild archive -project "${PROJ_NAME}.xcodeproj" -scheme ${SCHEME_NAME} -xcconfig build-dir.xcconfig
+
+CURRENT_DIR=`pwd`
+
+cd ${ARCH_XCODE_FOLDER}
+cd ${ARCH_DATE_FOLDER}
+
+LATEST_FILE=`ls -rt | tail -1`
+cp -R "${LATEST_FILE}" "${IPA_LOC}"
 
 # ok - ori
 #xcodebuild -target ${PROJ_NAME} -sdk iphoneos -configuration ${CONFIGURATION}
